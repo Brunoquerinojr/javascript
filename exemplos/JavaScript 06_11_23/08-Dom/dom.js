8df3d098e5a11c1d8c049129826c2b50fdf37101
Brunoquerinojr <brunoquerinosilvajr@gmail.com>
2023-11-16 09:43:54 -0300

    usandodom

--- exemplos/JavaScript 06_11_23/08-Dom/dom.js
+++ exemplos/JavaScript 06_11_23/08-Dom/dom.js
@@ -1,29 +1,37 @@
 // Troca a imagem para (Velozes e Furiosos)
 
 
-// Encontre o elemento <img> pelo atributo alt
-const imgElement = document.querySelector('img[alt="DOM"]');
+// // Encontre o elemento <img> pelo atributo alt
+// const imgElement = document.querySelector('img[alt="DOM"]');
 
-// Defina o caminho local da imagem
-// const caminhoLocal = 'img/A_Veloz.jpg';
-const caminhoLocal = 'img/B_Veloz.jpg';
+// // Defina o caminho local da imagem
+// // const caminhoLocal = 'img/A_Veloz.jpg';
+// const caminhoLocal = 'img/B_Veloz.jpg';
 
-// Altere o atributo src do elemento <img> para o caminho local
-imgElement.setAttribute('src', caminhoLocal);
+// // Altere o atributo src do elemento <img> para o caminho local
+// imgElement.setAttribute('src', caminhoLocal);
 
-// ____________________________________________________________________________
+// // ____________________________________________________________________________
 
-/* Métodos/Funções de acesso e seleção
+// /* Métodos/Funções de acesso e seleção
 
-getElementById() -> seleciona um elemento pelo ID
-querySelector() -> seleciona UM elemento de acordo com um seletor
-querySelectorAll() -> seleciona VÁRIOS elementos de acordo com um seletor */
+// getElementById() -> seleciona um elemento pelo ID
+// querySelector() -> seleciona UM elemento de acordo com um seletor
+// querySelectorAll() -> seleciona VÁRIOS elementos de acordo com um seletor */
 
 // ____________________________________________________________________________
 // Modificando elementos
 
 
 // 1ª Digitação (Aqui)
+// Selecionando pelo Id
+const titulo = document.getElementById('titulo-principal');
+// Selecionando pela TAG
+const pagina = document.querySelector('body');
+// Selecionando todas as TAgG h2
+const subtitulo = document.querySelectorAll('h2');
+
+const legenda = document.querySelector('figcaption');
 
 
 // ____________________________________________________________________________
@@ -32,8 +40,17 @@ querySelectorAll() -> seleciona VÁRIOS elementos de acordo com um seletor */
 
 
 // 2ª Digitação (Aqui)
+// H1
+titulo.textContent ="Ola mundo";
 
+// Cor de funfo (BOdy)
+pagina.style.backgroundColor = "green";
 
+// Mudando o segundo h2
+subtitulo[1].textContant="Full-Stack"
+
+//Mudando a legenda (figcaption)
+legenda.innerHTML="<b>Veloz e Furioso</br>"
 // ____________________________________________________________________________
 
 // CSS vis JS
@@ -54,9 +71,9 @@ const subtitulos = document.querySelectorAll('h2');
 // });
 
 // Sintaxe For...of (Loop comum)
-// for (const subtitulo of subtitulos) {
-//     subtitulo.style.color = 'blue';
-//   }
+for (const subtitulo of subtitulos) {
+    subtitulo.style.color = 'blue';
+  }
 
 
 // ____________________________________________________________________________
@@ -67,10 +84,21 @@ const subtitulos = document.querySelectorAll('h2');
 
 
 // 3ª Digitação (Aqui)
-
+const tituloEditores = document.querySelector("h3");
+tituloEditores.textContent = 'Melhores editores';
+tituloEditores.style.color = 'orange';
+titulo.style.fontSize = '28px';
 
 // Adicionar um novo editor à lista:
 
+const listaEditores = document.querySelector('#lista-editores');
+const novoEditor = document.createElement('li');
+
+novoEditor.textContent = 'Sublime';
+listaEditores.appendChild(novoEditor);
+
+const terceiroItem = listaEditores.querySelectorAll('li')[2];
+terceiroItem. style.color = 'orange';
 
 // 4ª Digitação (Aqui)
 
